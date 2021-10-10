97b4b8e80481974503766b44c9702b762d810115
jcob95 <jcob95@gmail.com>
2021-10-10 21:25:24 +0100

    enabled better pop up closing functionality and retrieving form data

--- library.js
+++ library.js
@@ -1,11 +1,43 @@
 
 const addBookBtn = document.getElementById('book-btn');
 const closeBtn = document.querySelector('.close');
+const container = document.querySelector('.container');
+const bgModal = document.querySelector('.bg-modal')
+const bookform = document.querySelector('.book-form');
 
-addBookBtn.addEventListener('click',() => {addBookToLibrary()});
-closeBtn.addEventListener('click',function() {
-    document.querySelector('.bg-modal').style.display = 'none';
+addBookBtn.addEventListener('click',() => {openModal()});
+closeBtn.addEventListener('click',() => {closeModal()})
+bgModal.addEventListener('click',function(event) {
+    //if you click on anything except the modal content itself or the "open modal" link, close the modal
+    if(!event.target.closest('.modal-content')){
+        closeModal();
+    }
+  })
+bookform.addEventListener('submit',function(event){
+    event.preventDefault();
+    //console.log("form submitted");
+    //console.log(bookform.elements['book-title'].value);
+    var formData = new FormData(bookform);
+    data = {};
+    for (var p of formData) {
+        let name = p[0];
+        let value = p[1];
+
+       data[name] = value;
+    }
+    addBookToLibrary(data);
+    closeModal();
 })
+function closeModal(){
+    document.querySelector('.bg-modal').style.display = 'none';
+    bookform.reset();
+}
+function addBookToLibrary(data){
+    console.log(data);
+}
+function openModal() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+}
 class library{
     constructor(){
         this.books = [];
@@ -30,8 +62,6 @@ let myLibrary = new library();
 myLibrary.books.push(new Book("book 1", "joe bloggs 1", "100",true));
 myLibrary.books.push(new Book("book 2", "joe bloggs 2", "200",false));
 
-function addBookToLibrary() {
-    document.querySelector('.bg-modal').style.display = 'flex';
-}
+
 
 
